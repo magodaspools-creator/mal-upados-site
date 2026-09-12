707a16319c94ef802ec9ff80cf8c691235746857
magodaspools-creator <magodaspools@gmail.com>
2026-09-12 17:42:56 -0300

    fix(godot): prevent held keys from repeating potion and upgrades

--- arena-godot/scripts/arena.gd
+++ arena-godot/scripts/arena.gd
@@ -28,10 +28,6 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
     if Input.is_action_just_pressed("attack"):
         attack()
-    if Input.is_key_pressed(KEY_Q):
-        _use_potion()
-    if Input.is_key_pressed(KEY_U):
-        _upgrade_weapon()
 
 func start_wave(next_wave: int = wave) -> void:
     if player_hp <= 0:
