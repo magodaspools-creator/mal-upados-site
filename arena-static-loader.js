87b104ecf0aaa6781ec52bd7edf9bf004322c5b1
magodaspools-creator <magodaspools@gmail.com>
2026-09-12 20:51:37 -0300

    Add static mob loader

---
+++ arena-static-loader.js
@@ -0,0 +1,2 @@
+// Load static mob renderer without changing arena.html manually.
+(()=>{const s=document.createElement('script');s.src='arena-game-static-mobs-v2.js?v=20260912';document.head.appendChild(s)})();
