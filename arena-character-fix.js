fd03ad1c5bd1b2f01191389f20701581d3f02427
magodaspools-creator <magodaspools@gmail.com>
2026-09-12 11:39:52 -0300

    fix: load infinite bestiary counters

--- arena-character-fix.js
+++ arena-character-fix.js
@@ -43,9 +43,9 @@
 (()=>{
   const files=[
     'arena-campaign-timer-fix.js?v=timer-fix-20260912c',
-    'arena-bestiary-fix.js?v=bestiary-fix-20260912e',
+    'arena-bestiary-fix.js?v=bestiary-infinite-20260912a',
     'arena-bestiary-intro.js?v=bestiary-intro-20260912b',
-    'arena-bestiary-counter-fix.js?v=bestiary-counter-20260912a',
+    'arena-bestiary-counter-fix.js?v=bestiary-counter-infinite-20260912a',
     'arena-bestiary-platinum-popup.js?v=platinum-popup-20260912a',
     'arena-combat-final-fix.js?v=combat-fix-20260912',
     'arena-flee-fix.js?v=flee-fix-20260912',
