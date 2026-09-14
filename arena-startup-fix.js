d1999b5d907938daf1a73c583060810ea7a5ee3c
magodaspools-creator <magodaspools@gmail.com>
2026-09-14 18:22:14 -0300

    chore(arena): bump forge dungeon cache version

--- arena-startup-fix.js
+++ arena-startup-fix.js
@@ -9,7 +9,7 @@
   function loadPolish(){loadScript('arena-ui-polish.js?v=ui-polish-20260914a','data-arena-ui-polish')}
   function loadSubnavFix(){loadScript('arena-subnav-fix.js?v=subnav-fix-20260914','data-arena-subnav-fix')}
   function loadViews(){loadScript('arena-views.js?v=views-20260914','data-arena-views')}
-  function loadForgeV2(){loadScript('arena-forge-v2.js?v=forge-v2-20260914','data-arena-forge-v2')}
+  function loadForgeV2(){loadScript('arena-forge-v2.js?v=forge-v2-20260914b','data-arena-forge-v2')}
 
   function boot(){
     if(typeof window.load==='function'){
